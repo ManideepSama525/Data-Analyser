c2d92c4d8491cc10115352254727c4ea3c57ada2
ManideepSama525 <manideep2009009@gmail.com>
2025-06-11 01:38:15 +0530

    Update app_final_google_sheets_secrets.py

--- app_final_google_sheets_secrets.py
+++ app_final_google_sheets_secrets.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+from google.oauth2client.service_account import ServiceAccountCredentials
 import bcrypt
 import pandas as pd
 import matplotlib.pyplot as plt
